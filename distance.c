33190bdbf7554faadef1ec0f4d6a98a029d4537b
HAMADSAALEH <hamadsalehswabi@gmail.com>
2023-10-22 18:43:24 +0500

    distance.c 2

--- distance.c
+++ distance.c
@@ -6,4 +6,14 @@ double degreesToRadians(double degrees)
     return degrees * 3.14 / 180.0;
 }
 double haversine(double lat1, double lon1, double lat2, double lon2)
-{
+{ 
+     lat1 = degreesToRadians(lat1);
+    lon1 = degreesToRadians(lon1);
+    lat2 = degreesToRadians(lat2);
+    lon2 = degreesToRadians(lon2);
+
+    double dlat = lat2 - lat1;
+    double dlon = lon2 - lon1;
+    double a = sin(dlat / 2.0) * sin(dlat / 2.0) + cos(lat1) * cos(lat2) * sin(dlon / 2.0) * sin(dlon / 2.0);
+    double c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a));
+}
