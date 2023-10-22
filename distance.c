56cbe078bd890954b282f71253ee723d46954124
HAMADSAALEH <hamadsalehswabi@gmail.com>
2023-10-22 18:48:16 +0500

    distance.c3

--- distance.c
+++ distance.c
@@ -17,3 +17,12 @@ double haversine(double lat1, double lon1, double lat2, double lon2)
     double a = sin(dlat / 2.0) * sin(dlat / 2.0) + cos(lat1) * cos(lat2) * sin(dlon / 2.0) * sin(dlon / 2.0);
     double c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a));
 }
+   int main()
+    {
+        double lat1, lon1, lat2, lon2;
+
+        printf("Enter the latitude and longitude of the first location (in degrees):\n");
+        printf("Latitude 1 (-90 to 90): ");
+        scanf("%lf", &lat1);
+        printf("Longitude 1 (-180 to 180): ");
+        scanf("%lf", &lon1);
